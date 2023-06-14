72ba6f4a42e1f7810ad376c1241a5f1c0c34065e
MCaellasp <136328927+MCaellasp@users.noreply.github.com>
2023-06-14 09:50:13 +0000

    first commit

---
+++ R_Producte-Proveidor.sql
@@ -0,0 +1,35 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.3
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: proxysql-01.dd.scip.local
+-- Temps de generació: 14-06-2023 a les 11:27:51
+-- Versió del servidor: 10.10.2-MariaDB-1:10.10.2+maria~deb11
+-- Versió de PHP: 8.1.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de dades: `ddb203467`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de la taula `R Producte-Proveidor`
+--
+-- Error en llegir l'estructura per a la taula ddb203467.R Producte-Proveidor: #1146 - Table &#039;ddb203467.R Producte-Proveidor&#039; doesn&#039;t exist
+-- Error en llegir les dades per a la taula ddb203467.R Producte-Proveidor: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `ddb203467`.`R Producte-Proveidor`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
